b3143ddacf8bce24df3737a8a99f59aeda80350e
arkyan24 <aridhop24@gmail.com>
2024-12-27 13:32:02 +0700

    Update Penyesuaian

--- PengawasProyek.java
+++ PengawasProyek.java
@@ -1,13 +1,12 @@
 import java.util.Date;
 
 class PengawasProyek extends TimPekerja {
-    public PengawasProyek(String id, String nama) {
-        super(id, nama);
+    public PengawasProyek(String id, String nama, Date tanggalMasuk, String detail) {
+        super(id, nama, tanggalMasuk, detail);
     }
 
     @Override
     public int Gaji(int durationProject) {
         return 5000000 + (durationProject * 200000);
     }
-    
 }
