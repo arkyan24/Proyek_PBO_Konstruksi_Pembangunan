cab900262773b42b75962bd26bef418a2370245a
arkyan24 <aridhop24@gmail.com>
2024-12-27 13:33:36 +0700

    Update Penyesuaian

--- TenagaKerja.java
+++ TenagaKerja.java
@@ -1,13 +1,12 @@
 import java.util.Date;
 
 class TenagaKerja extends TimPekerja {
-    public TenagaKerja(String id, String nama) {
-        super(id, nama);
+    public TenagaKerja(String id, String nama, Date tanggalMasuk, String detail) {
+        super(id, nama, tanggalMasuk, detail);
     }
 
     @Override
     public int Gaji(int durationProject) {
         return 3000000 + (durationProject * 100000);
     }
-    
 }
