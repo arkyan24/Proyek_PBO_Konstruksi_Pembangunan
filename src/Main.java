d5a7af1f4efe97ecb88fe799fc84b7e13d7c2fd5
arkyan24 <aridhop24@gmail.com>
2025-01-13 22:26:43 +0700

    Penyesuaian

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@ import java.text.SimpleDateFormat;
 import java.util.Scanner;
 import java.lang.String;
 
-
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -363,5 +362,4 @@ public class Main {
         }
     }
 
-    
 }
